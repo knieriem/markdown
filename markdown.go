eb8169a8e58c86417f2717c9948eec2a369eb0cb
Michael Teichgräber <mt@ib.wmipf.de>
2012-04-30 01:14:31 +0200

    Require newline after Reference. [jgm/peg-markdown]
    See https://github.com/jgm/peg-markdown/commit/fff88fe

--- markdown.go
+++ markdown.go
@@ -31,7 +31,7 @@ const (
 	// rebuild it using
 	//	make nuke
 	//	make parser
-	needParserIfaceVersion = parserIfaceVersion_3
+	needParserIfaceVersion = parserIfaceVersion_4
 )
 
 // Markdown Options:
