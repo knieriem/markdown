ba2d82f61697b4c4e81d7335250fadabd65fe853
Michael Teichgräber <mt@ib.wmipf.de>
2012-04-30 10:13:12 +0200

    Simplified SkipBlock. [jgm/peg-markdown]
    See https://github.com/jgm/peg-markdown/commit/85439dd

--- markdown.go
+++ markdown.go
@@ -31,7 +31,7 @@ const (
 	// rebuild it using
 	//	make nuke
 	//	make parser
-	needParserIfaceVersion = parserIfaceVersion_13
+	needParserIfaceVersion = parserIfaceVersion_14
 )
 
 // Markdown Options:
