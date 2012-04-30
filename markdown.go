5bebe88174668d4936b342557741857a5c96f70d
Michael Teichgräber <mt@ib.wmipf.de>
2012-04-30 10:14:27 +0200

    Fixed exponential problem with Emph/Strong. [jgm/peg-markdown]
    See https://github.com/jgm/peg-markdown/commit/c258a9a

--- markdown.go
+++ markdown.go
@@ -31,7 +31,7 @@ const (
 	// rebuild it using
 	//	make nuke
 	//	make parser
-	needParserIfaceVersion = parserIfaceVersion_14
+	needParserIfaceVersion = parserIfaceVersion_15
 )
 
 // Markdown Options:
