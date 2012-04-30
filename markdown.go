5f0135f259dcf0ca4f0fdabe3a1a173e8860e96d
Michael Teichgräber <mt@ib.wmipf.de>
2012-04-30 10:10:32 +0200

    Improved AutoLinkEmail (Fletcher Penney). [jgm/peg-markdown]
    See https://github.com/jgm/peg-markdown/commit/ca4df46

--- markdown.go
+++ markdown.go
@@ -31,7 +31,7 @@ const (
 	// rebuild it using
 	//	make nuke
 	//	make parser
-	needParserIfaceVersion = parserIfaceVersion_11
+	needParserIfaceVersion = parserIfaceVersion_12
 )
 
 // Markdown Options:
