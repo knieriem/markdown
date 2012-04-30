982e5856bf9fc3e57665629850569c73f1d790a2
Michael Teichgräber <mt@ib.wmipf.de>
2012-04-30 10:16:52 +0200

    Same as previous patch but for the underlined variant. [jgm/peg-markdown]
    See https://github.com/jgm/peg-markdown/commit/2a5daec

--- markdown.go
+++ markdown.go
@@ -31,7 +31,7 @@ const (
 	// rebuild it using
 	//	make nuke
 	//	make parser
-	needParserIfaceVersion = parserIfaceVersion_15
+	needParserIfaceVersion = parserIfaceVersion_16
 )
 
 // Markdown Options:
